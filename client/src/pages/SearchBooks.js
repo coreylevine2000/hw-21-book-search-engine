03e21e899dd95355f9fb21b48310737711470857
Corey Levine <coreylevine2000@gmail.com>
2022-07-24 21:09:58 -0400

    removing unneeded imports

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/react-hooks';
 import { SAVE_BOOK } from '../utils/mutations';
@@ -32,7 +31,7 @@ const SearchBooks = () => {
 
     try {
       const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
-      
+
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
